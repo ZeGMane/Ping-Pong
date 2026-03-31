384836ea6c3160d33ce001a29adf1802e24d9f02
ZeGMane <zenaa1725@gmail.com>
2026-03-31 19:04:13 +0300

    Добавил мяч и ракетки с реализованным функционалом

--- main.py
+++ main.py
@@ -5,13 +5,59 @@ display.set_caption('Ping-Pong')
 
 bg = transform.scale(image.load('bg.png'), (600,500))
 
+class GameSprite(sprite.Sprite):
+    def __init__(self, image, x, y, speed):
+        super().__init__()
+        self.image = image
+        self.speed = speed
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+    def reset(self):
+        win.blit(self.image,(self.rect.x, self.rect.y))
+        
+class Player(GameSprite):
+    def update_left(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y >= 5:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y <= 355:
+            self.rect.y += self.speed
+    
+    def update_right(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y >= 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y <= 355:
+            self.rect.y += self.speed
+            
+raketka_sprite = transform.scale(image.load('raletka.png'), (30, 140))
+ball_sprite = transform.scale(image.load('ball.png'), (40,40))
+
+
+raketka1 = Player(raketka_sprite, 20, 100, 8)
+raketka2 = Player(raketka_sprite, 550, 100, 8)
+
+ball = GameSprite(ball_sprite, 300, 250, 10)
+
+clock = time.Clock()
+
 game = True
 while game:
     win.blit(bg, (0,0))
+    
+    raketka1.update_left()
+    raketka1.reset()
+    
+    raketka2.update_right()
+    raketka2.reset()
+    
+    ball.reset()
     
     for e in event.get():
         if e.type == QUIT:
             game = False
             
+    clock.tick(60)
     display.update()
             
